96e164da50482b9ffe5c3bb5f810a0c6993ffc9a
vineeth boppudi <50747849+vineethboppudi@users.noreply.github.com>
2020-01-30 10:50:04 +0530

    Delete write.js

--- write.js
+++
@@ -1,9 +0,0 @@
- const fs = require("fs");
-  let writestream = fs.createWriteStream('out.csv');
-
-   writestream.write("sghjghjnvnn","base64");
-   writestream.on( 'finish', () =>{
-       console.log("wrote all the data");
-
-})
-writestream.end();
